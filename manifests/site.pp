fc52b91c84d3ddb1358e9b6b318da4c667bf7760
freeflychi <52017169+freeflychi@users.noreply.github.com>
2021-06-16 15:07:37 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,14 +1,5 @@
 node default {
-  }
+}
 node 'master.puppet.vm' {
   include role::master_server
 }
-file {'/root/README':
-3
-    ensure  => file,
-4
-    content => 'Test readme',
-5
-    owner   => 'root',
-6
-  }
