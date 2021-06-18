731ed311c695a2d085d3f0ca15e9b699fde07eb4
freeflychi <52017169+freeflychi@users.noreply.github.com>
2021-06-18 13:05:32 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,4 +2,15 @@ node default {
 }
 node 'master.puppet.vm' {
   include role::master_server
+  file { '/root/README':
+    ensure => file,
+    content => $fqdn,
+    }
+}
+node /^web/ {
+  inclide role::app_server
+}
+
+node /^db/ {
+  inclide role::db_server
 }
